3951d52e354d93b479087b2ac71fcbb54223689a
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-04 16:57:45 +0530

    'psdview'

--- src/Components/View.js
+++ src/Components/View.js
@@ -1,25 +1,30 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+
 const View = () => {
     const location = useLocation();
-  const pdfUrl = location.state?.pdfUrl;
-  const pdf = `https://aktu-quantum-backend.onrender.com/public/temp/${pdfUrl}`
-
-  return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-   <div
-    style={{
-        border: '1px solid rgba(0, 0, 0, 0.3)',
-        height: '750px',
-    }}
->
-    <Viewer fileUrl={pdf} />
-</div>
+    const [pdfUrl, setPdfUrl] = useState('');
     
-</Worker>
-  )
-}
+    useEffect(() => {
+        // Fetch the PDF URL when the component mounts
+        const fetchPdfUrl = async () => {
+            const pdfUrl = location.state?.pdfUrl;
+            if (pdfUrl) {
+                setPdfUrl(`https://aktu-quantum-backend.onrender.com/public/temp/${pdfUrl}`);
+            }
+        };
+        fetchPdfUrl();
+    }, [location.state?.pdfUrl]);
 
-export default View
+    return (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)', height: '750px' }}>
+                {pdfUrl && <Viewer fileUrl={pdfUrl} />}
+            </div>
+        </Worker>
+    );
+};
+
+export default View;
