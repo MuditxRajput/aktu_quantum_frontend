4d43fb2289ef3bb79b4c45bf73f35da794fbf3fa
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-05 20:55:09 +0530

    'enableDownload'

--- src/Components/View.js
+++ src/Components/View.js
@@ -1,12 +1,16 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import styles
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const View = () => {
     const location = useLocation();
     const [pdfUrl, setPdfUrl] = useState('');
-    
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     useEffect(() => {
         // Fetch the PDF URL when the component mounts
         const fetchPdfUrl = async () => {
@@ -15,13 +19,14 @@ const View = () => {
                 setPdfUrl(`https://aktu-quantum-backend.onrender.com/public/temp/${pdfUrl}`);
             }
         };
+       
         fetchPdfUrl();
     }, [location.state?.pdfUrl]);
 
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
             <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)', height: '750px' }}>
-                {pdfUrl && <Viewer fileUrl={pdfUrl} />}
+                {pdfUrl && <Viewer fileUrl={pdfUrl} plugins={[defaultLayoutPluginInstance]} />}
             </div>
         </Worker>
     );
