55d217ffbe77ad5cff2da74bf52551db427bfda6
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-14 20:05:54 +0530

    'seo3'

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -28,16 +28,12 @@ const Hero = () => {
     <div className="flex flex-col sm:flex-row  h-[450px]">
       <div className=" p-1  flex flex-col flex-1   justify-center items-center">
         <div className=" flex flex-col gap-5 justify-center ">
-          <p className=" text-3xl sm:text-5xl">
-          {/* "Free AKTU Quantum Study Guides – Ace Your Exams Last Minute | StudyHelp" */}
+          <h1 className=" text-3xl sm:text-5xl ">
           Free<span className="text-orange-600 hover:text-orange-700 "> AKTU Quantum</span>  <br></br>Study Guides <br></br>Ace Your Exams <br></br> Last Minute.
-          </p>
+          </h1>
           <h2 className=" text-sm sm:text-md">
           Dive into AKTU quantum pdf with our website, offering free AKTU quantum PDFs.<br></br> Explore comprehensive resources for free quantum PDFs. <br></br>Get your AKTU quantum solutions hassle-free!<br></br> Get all year aktu quantum pdf download.
           </h2>
-          {/* <div className="flex">
-            <HeaderButton  text="Sign Up" />
-          </div> */}
         </div>
       </div>
       <div className="flex flex-1  justify-center items-center">
