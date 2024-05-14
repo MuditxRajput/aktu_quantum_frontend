563068f8c978c985813868ca7b1a8d710163ac45
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-14 19:58:37 +0530

    'seo2'

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -28,10 +28,10 @@ const Hero = () => {
     <div className="flex flex-col sm:flex-row  h-[450px]">
       <div className=" p-1  flex flex-col flex-1   justify-center items-center">
         <div className=" flex flex-col gap-5 justify-center ">
-          <h1 className=" text-3xl sm:text-5xl">
+          <p className=" text-3xl sm:text-5xl">
           {/* "Free AKTU Quantum Study Guides – Ace Your Exams Last Minute | StudyHelp" */}
           Free<span className="text-orange-600 hover:text-orange-700 "> AKTU Quantum</span>  <br></br>Study Guides <br></br>Ace Your Exams <br></br> Last Minute.
-          </h1>
+          </p>
           <h2 className=" text-sm sm:text-md">
           Dive into AKTU quantum pdf with our website, offering free AKTU quantum PDFs.<br></br> Explore comprehensive resources for free quantum PDFs. <br></br>Get your AKTU quantum solutions hassle-free!<br></br> Get all year aktu quantum pdf download.
           </h2>
