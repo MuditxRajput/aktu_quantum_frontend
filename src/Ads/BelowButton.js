db84bd869328cee1fa488591960e6f4ced9ac1ae
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-22 19:51:10 +0530

    'changes'

--- src/Ads/BelowButton.js
+++ src/Ads/BelowButton.js
@@ -1,21 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 const BelowButton = () => {
-    const [isUnfilled, setIsUnfilled] = useState(false);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const adElement = document.querySelector('.adsbygoogle');
-            if (adElement && adElement.getAttribute('data-ad-status') === 'unfilled') {
-                setIsUnfilled(true);
-            } else {
-                setIsUnfilled(false);
-            }
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     useEffect(() => {
         try {
             (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -26,7 +11,7 @@ const BelowButton = () => {
 
     return (
         <div>
-            <ins className={`adsbygoogle ${isUnfilled ? 'hidden' : ''}`}
+            <ins className="adsbygoogle"
                  style={{ display: "block" }}
                  data-ad-format="autorelaxed"
                  data-ad-client="ca-pub-8884977910314227"
